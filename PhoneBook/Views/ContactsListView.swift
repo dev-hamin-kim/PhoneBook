898af58ed3897876d45bcd5e8c3042b141e46d02
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-09 22:13:36 +0900

    [wip] working on features in Lv.1 to Lv.3

---
+++ PhoneBook/Views/ContactsListView.swift
@@ -0,0 +1,26 @@
+//
+//  ContactsListView.swift
+//  PhoneBook
+//
+//  Created by 김하민 on 12/6/24.
+//
+
+//import UIKit
+//
+//final class ContactsListView: UINavigationItem {
+//
+//    private let navigationBar: UINavigationBar = {
+//        let navigationBar = UINavigationBar()
+//        navigationBar.largeContentTitle = "친구 목록"
+//        return navigationBar
+//    }()
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: .zero)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//}
