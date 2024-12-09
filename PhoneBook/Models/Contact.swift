f34449bb68c2f4559e3237a97ab820a3a8823b4e
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-09 14:21:46 +0900

    [new] Finished ContactsTableView created struct Contact and static var Contacts for temporary use linked ContactsTableViewCell and ContactsTableView

---
+++ PhoneBook/Models/Contact.swift
@@ -0,0 +1,19 @@
+//
+//  Contact.swift
+//  PhoneBook
+//
+//  Created by 김하민 on 12/9/24.
+//
+
+struct Contact {
+    let name: String
+    let number: String
+    
+    static var Contacts: [Contact] = [
+        Contact(name: "가나다", number: "010-2222-3333"),
+        Contact(name: "Alex", number: "010-1414-2424"),
+        Contact(name: "Betty", number: "010-5345-2323")
+    ]
+}
+
+
---
+++ PhoneBook/Views/ContactsTableView.swift
@@ -0,0 +1,56 @@
+//
+//  ContactsTableView.swift
+//  PhoneBook
+//
+//  Created by 김하민 on 12/6/24.
+//
+
+import UIKit
+import SnapKit
+
+final class ContactsTableView: UIView, UITableViewDataSource, UITableViewDelegate {
+
+    
+    private let tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setTableView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setTableView() {
+        tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: ContactsTableViewCell.id)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .singleLine
+        tableView.rowHeight = 80
+        self.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Contact.Contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ContactsTableViewCell.id, for: indexPath) as! ContactsTableViewCell
+        cell.setCellData(with: Contact.Contacts[indexPath.row])
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension ContactsTableView: ContactsTableViewCellDelegate {
+    
+}
