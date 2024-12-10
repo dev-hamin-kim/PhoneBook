822def40c232492e8b31062538044770b5e8930a
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-10 11:05:22 +0900

    [feat] Completed Lv.1 and NavigationBar

--- PhoneBook/Controllers/MainViewController.swift
+++ PhoneBook/Controllers/MainViewController.swift
@@ -10,58 +10,28 @@ import SnapKit
 
 final class MainViewController: UIViewController {
     
-    let contactsTableView: ContactsTableView = .init()
-    
-    private let navItem: UINavigationItem = {
-        let item = UINavigationItem()
-        item.title = "친구 목록"
-        return item
-    }()
-    
-    private let navigationBar: UINavigationBar = {
-        let navigationBar = UINavigationBar()
-        navigationBar.barStyle = .default
-        navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
-        return navigationBar
-    }()
+    private lazy var contactsTableView: ContactsTableView = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(navigationBar)
-        view.addSubview(contactsTableView)
-        
         setNavigationBar()
-        setConstraints()
+        view = contactsTableView
     }
     
     private func setNavigationBar() {
-        navigationBar.setItems([navItem], animated: true)
-        navItem.rightBarButtonItem = UIBarButtonItem(title: "추가",
+        navigationItem.title = "친구 목록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가",
                                                      style: .plain,
-                                                     target: MainViewController(),
+                                                     target: self,
                                                      action: #selector(onTap))
     }
     
-    private func setConstraints() {
-        let safeArea = self.view.safeAreaLayoutGuide
-        
-        navigationBar.snp.makeConstraints { make in
-            make.top.equalTo(safeArea.snp.top)
-            make.leading.trailing.equalToSuperview()
-        }
-        
-        contactsTableView.snp.makeConstraints { make in
-            make.top.equalTo(navigationBar.snp.bottom)
-            make.bottom.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
-        }
-    }
-    
+}
+
+extension MainViewController {
     @objc private func onTap() {
         print("탭")
     }
-    
 }
 
 #Preview {
--- PhoneBook/SceneDelegate.swift
+++ PhoneBook/SceneDelegate.swift
@@ -17,9 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene:  windowScene)
-        window?.rootViewController = MainViewController()
-        window?.makeKeyAndVisible()
+        
+        let window = UIWindow(windowScene:  windowScene)
+        let mainVC = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        
+        window.rootViewController = navigationController
+        window.backgroundColor = .systemBackground
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
